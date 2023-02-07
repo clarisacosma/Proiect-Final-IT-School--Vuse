669c772937898c1941ae57da290cd14cbbd96285
clarisacosma <cosma.clarisa@gmail.com>
2023-02-07 17:45:47 +0200

    js features

--- functions.js
+++ functions.js
@@ -3,11 +3,40 @@ const navBar = document.querySelector(".nav2");
 const supportBtn = document.querySelector(".supportBtn");
 const supportTooltip = document.createElement("div");
 const shade = document.getElementById("shade");
+const account = document.querySelector(".account");
+const navRight = document.querySelector(".navRight");
+const shop = document.querySelector(".shop");
+let accountInfo = document.createElement("form");
 
+// Log in Form
+account.addEventListener("click", () => {
+  if (!accountInfo) {
+    accountInfo = document.createElement("form");
+    accountInfo.innerHTML = `<div class="loginNameContainer">
+      <label for="loginName">Email:</label>
+      <input type="email" id="loginName" placeholder="email address" required>
+      </div>
+      <div class="loginPassContainer">
+      <label for="loginPassword">Password:</label>
+      <input type="password" id="loginPassword" placeholder="password" required>
+      </div>
+      <button>Submit</button>
+`;
+    accountInfo.classList.add("logIn");
+    accountInfo.setAttribute("action", "GET");
+    navRight.insertBefore(accountInfo, shop);
+  } else {
+    accountInfo.remove();
+    accountInfo = null;
+  }
+});
+
+// Hamburger Menu
 hamburger.addEventListener("click", () => {
   navBar.classList.toggle("active");
 });
 
+// Support Toggle
 supportBtn.addEventListener("mouseover", () => {
   supportTooltip.innerHTML = `<div class="contact">
   <h2>CONTACT</h2>
